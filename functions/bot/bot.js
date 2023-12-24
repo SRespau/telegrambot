de76f42f82d3726f463b5472034d83ab28b70003
SRespau <sergiorespau@gmail.com>
2023-12-24 20:27:48 +0100

    Added Aragonia and Palafox. Need clean up and refactor

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -2,8 +2,12 @@ const https = require('https');
 const { Telegraf, Markup } = require("telegraf");
 const axios = require('axios')
 const cheerio = require('cheerio')
+const puppeteer = require('puppeteer')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+//Variables globales
+let cineSeleccionadoGlobal = null
 
 // FUNCIONES A MOVER
 function removeTextSigns(text) {
@@ -11,6 +15,64 @@ function removeTextSigns(text) {
   modifiedText = modifiedText.charAt(0).toUpperCase() + modifiedText.slice(1)
   return modifiedText
 }
+
+// PUPPETEER PARA CINESA PVENECIA (MOVER A FICHERO A PARTE)
+async function clickSeeMoreButtons() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navegar a la página
+  await page.goto('https://www.filmaffinity.com/es/theater-showtimes.php?id=530', {waitUntil: 'load'});
+  // Aceptamos las cookies
+  const acceptCookiesButton = await page.$('[class=" css-v43ltw"]')
+  if (acceptCookiesButton) {
+    if (acceptCookiesButton) {
+      await acceptCookiesButton.click();
+    }
+  }
+  // Seleccionar y hacer clic en todos los botones con la clase "see-more"
+  await page.waitForSelector('.see-more', { visible: true });
+  const seeMoreButtons = await page.$$('[class="see-more"]');
+  for (const button of seeMoreButtons) { 
+    await button.click();
+    await new Promise(resolve => setTimeout(resolve, 100)) 
+  }  
+  // Obtener el HTML actualizado después de hacer clic en los botones
+  const updatedHTML = await page.content()
+
+  // Usar Cheerio para analizar el HTML actualizado
+  const $ = cheerio.load(updatedHTML)
+  let titles = $('.mv-title')
+  titles.each((index, element) => {
+    // Obtener todos los nodos hijos, incluidos elementos y nodos de texto
+    const childNodes = $(element).contents()
+  
+    // Filtrar los nodos de texto
+    const textNodes = childNodes.filter(function() {
+      return this.nodeType === 3; // Node.TEXT_NODE
+    })
+  
+    // Recorrer los nodos de texto y mostrar su contenido
+    textNodes.each((index, textNode) => {
+      const textContent = textNode.nodeValue.trim();
+      if (textContent !== "") {
+        console.log(textContent);
+      }
+    });
+  });
+    // Extraer todos los li directos de la lista ul con clase ".sessions"
+  const sessionsLi = $('.sessions > li');
+
+  // Iterar sobre los li y mostrar su texto en la consola
+  sessionsLi.each((index, element) => {
+    const dataSessDate = $(element).attr('data-sess-date');
+    console.log(dataSessDate);
+  });
+
+  // Cerrar el navegador
+  await browser.close();
+}
+/////
 
 const cines = Markup.inlineKeyboard([
   // 1-st column
@@ -34,9 +96,11 @@ bot.command('cartelera', (ctx) => {
 bot.action(['aragonia', 'palafox', 'grancasa', 'pvenecia', 'arte7'], async (ctx) => {
   // ctx.match contiene el callback_data del botón presionado
   const cineSeleccionado = ctx.match[0];
-
+  cineSeleccionadoGlobal = cineSeleccionado
+  
   switch (cineSeleccionado) {
     case 'aragonia':
+    case 'palafox':
       // OBTENEMOS LOS TITULOS DE LA CARTELERA
       const response = await axios.get('https://www.cinespalafox.com/cartelera.html', {
         httpsAgent: new https.Agent({ rejectUnauthorized: false }) // Desactiva la verificación del certificado
@@ -69,7 +133,7 @@ bot.action(['aragonia', 'palafox', 'grancasa', 'pvenecia', 'arte7'], async (ctx)
           const value = match && match[1]
       
           // Crear el botón con el valor modificado
-          return Markup.button.callback(key, value);
+          return Markup.button.callback(removeTextSigns(key), value);
         });
       
         buttonColumns.push(columnButtons);
@@ -79,14 +143,11 @@ bot.action(['aragonia', 'palafox', 'grancasa', 'pvenecia', 'arte7'], async (ctx)
       ctx.editMessageText("Selecciona una opción:", inlineKeyboard);
 
       break;
-    case 'palafox':
-      ctx.editMessageText('Palafox seleccionado');
-      break;
     case 'grancasa':
       ctx.editMessageText('Cinesa (Gran Casa) seleccionado');
       break;
     case 'pvenecia':
-      ctx.editMessageText('Cinesa (P. Venecia) seleccionado');
+      clickSeeMoreButtons()
       break;
     case 'arte7':
       ctx.editMessageText('Arte 7 seleccionado');
@@ -98,6 +159,8 @@ bot.action(['aragonia', 'palafox', 'grancasa', 'pvenecia', 'arte7'], async (ctx)
 
 //Leemos las peliculas con el link
 bot.action(/^(?!.*\.html).*$/, async (ctx) => {
+  console.log(ctx)
+  const cineSeleccionado = cineSeleccionadoGlobal
   const URL = `https://www.cinespalafox.com/cartelera/${ctx.match[0]}.html`
   // OBTENEMOS LOS TITULOS DE LA CARTELERA
   const response = await axios.get(URL, {
@@ -105,8 +168,8 @@ bot.action(/^(?!.*\.html).*$/, async (ctx) => {
   });
   const $ = cheerio.load(response.data);
   
-  const aragoniaDiv = $('.sesiones h3:contains("Cines aragonia")').parent();
-  const primerUl = aragoniaDiv.find('ul').first();
+  const cineDiv = $(`.sesiones h3:contains("Cines ${cineSeleccionado === "aragonia" ? "aragonia" : "Palafox"}")`).parent();
+  const primerUl = cineDiv.find('ul').first();
 
   const horariosData = {};
   // Iterar sobre los elementos li dentro de ul
@@ -119,15 +182,21 @@ bot.action(/^(?!.*\.html).*$/, async (ctx) => {
   });
 
   let renderHorario = '';
-  renderHorario += `${removeTextSigns(ctx.match[0])}\n`
-  for (const fecha in horariosData) {
-    renderHorario += `${fecha}:\n`;
-    horariosData[fecha].forEach(horario => {
-      renderHorario += `  ·${horario}\n`;
-    });
-    renderHorario += '\n';
+  renderHorario += `${removeTextSigns(ctx.match[0]).toUpperCase()}\n`
+  if(Object.keys(horariosData).length !== 0){
+    for (const fecha in horariosData) {
+      renderHorario += `${fecha}:\n`;
+      horariosData[fecha].forEach(horario => {
+        renderHorario += `  ·${horario}\n`;
+      });
+      renderHorario += '\n';
+    }
+  } else {
+    renderHorario += "No hay horarios para este cine"
   }
-  ctx.editMessageText(renderHorario)
+
+  ctx.editMessageText(`Aqui tienes los horarios para Cines ${cineSeleccionado}`)
+  ctx.reply(renderHorario)
 })
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
